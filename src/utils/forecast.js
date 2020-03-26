bad0974833bcb3fb4c3b007e51f1095897a0679b
Asmita Chavan <asmitachavan121@gmail.com>
2020-03-26 16:15:16 +0530

    added Highest temp and lowest temp of the day in forecast info

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,6 +2,13 @@ const urllib = require('urllib')
 
 const forecast = (latitude, longitude, callback) => {
     const url = 'https://api.darksky.net/forecast/592c45aa8a4445b37ab3e383b9689151/' + encodeURIComponent(latitude) + ',' + encodeURIComponent(longitude)
+    
+    function FerheheitToDegree(temp) {
+        temp = (temp - 32) * 0.55
+        temp = temp.toFixed(2)
+        return temp
+    }
+    
     urllib.request(url, (error, data, Response) => {
         if(data) {
             data = data.toString()
@@ -15,10 +22,14 @@ const forecast = (latitude, longitude, callback) => {
         }
          else {
             temp = data.currently.temperature
-            temp = (temp - 32) * 0.55
-            temp = temp.toFixed(2)
+            highTemp = data.daily.data[0].temperatureHigh
+            lowTemp = data.daily.data[0].temperatureLow
+            temp = FerheheitToDegree(temp)
+            highTemp = FerheheitToDegree(highTemp)
+            lowTemp = FerheheitToDegree(lowTemp)
+
             // console.log(data.daily.data[0].summary + ' It is currently '+ temp + ' degree temperature')
-            const forecastData = data.daily.data[0].summary + ' It is currently '+ temp + ' degree temperature'
+            const forecastData = data.daily.data[0].summary + ' It is currently '+ temp + ' degree temperature. The highest and lowest temperature of the day is nearly ' + highTemp + ' and ' + lowTemp + ' respectively.'
 
             callback(undefined, forecastData)
             //console.log(data.daily.data[0].summary + ' It is currently '+ data.currently.temperature + ' temperature')
@@ -26,6 +37,8 @@ const forecast = (latitude, longitude, callback) => {
 
         }
     })
+
+
 }
 
 
